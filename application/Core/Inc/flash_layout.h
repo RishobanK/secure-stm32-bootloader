04fbe4b619726d2bd6bd47369858e626736a7274
RishobanK <rishobankandeepan@gmail.com>
2026-05-27 15:40:54 +0530

    Add STM32 bootloader and application firmware projects

---
+++ application/Core/Inc/flash_layout.h
@@ -0,0 +1,12 @@
+#ifndef INC_FLASH_LAYOUT_H_
+#define INC_FLASH_LAYOUT_H_
+
+#define BL_START_ADDR			0X08000000
+
+#define APP_HEADER_ADDR			0x08004000
+
+#define APP_START_ADDR			0x08004400
+
+#define APP_MAX_SIZE			47*1024
+
+#endif /* INC_FLASH_LAYOUT_H_ */
---
+++ bootloader/Core/Inc/flash_layout.h
@@ -0,0 +1,12 @@
+#ifndef INC_FLASH_LAYOUT_H_
+#define INC_FLASH_LAYOUT_H_
+
+#define BL_START_ADDR			0X08000000	// 16KB
+
+#define APP_HEADER_ADDR			0x08004000	// 1KB
+
+#define APP_START_ADDR			0x08004400	// 47KB
+
+#define APP_MAX_SIZE			47*1024
+
+#endif /* INC_FLASH_LAYOUT_H_ */
